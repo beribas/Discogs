78d93b47b221c166226c579b388bc5b03e8ccf84
Oleg Langer <piligrimo@gmail.com>
2022-09-10 17:46:07 +0200

    Initial Commit

---
+++ Discogs/DiscogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscogsApp.swift
+//  Discogs
+//
+//  Created by Oleg Langer on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct DiscogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
